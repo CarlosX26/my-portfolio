0c2eaf770f67c88098b3ddd4bb584156589353c9
CarlosX26 <cjunior1@live.com>
2023-01-20 15:41:34 -0300

    feat: add alert on bot for start conversation

--- src/contexts/types.ts
+++ src/contexts/types.ts
@@ -2,10 +2,12 @@ import React from "react";
 import { iMessages } from "../components/ChatBot/types";
 
 export interface iPropsBotBrainContext {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export interface iBotBrainContext {
-  messages: iMessages[];
-  botDecisions: (message: iMessages) => void;
+    messages: iMessages[];
+    botDecisions: (message: iMessages) => void;
+    startConversation: boolean;
+    setStartConversation: React.Dispatch<React.SetStateAction<boolean>>;
 }
