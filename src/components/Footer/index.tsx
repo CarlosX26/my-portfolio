c5f2ec5799b377a3ccc087217e76a2a15268e91e
Carlos Junio <cjunior1@live.com>
2023-05-04 11:13:21 -0300

    refactor: remove styles and h1

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,33 +1,29 @@
 import { BsLinkedin, BsGithub } from "react-icons/bs"
 import { ContainerStyled } from "../../styles/container"
-import { HeadingStyledThree, HeadingStyledTwo } from "../../styles/typography"
+import { HeadingStyledThree } from "../../styles/typography"
 import { FooterStyled } from "./styles"
 
 export const Footer = () => {
   return (
     <FooterStyled>
       <ContainerStyled>
-        <div className="box-info">
-          <div>
-            <HeadingStyledTwo>Eu sou Carlos Junio</HeadingStyledTwo>
-            <HeadingStyledThree>Always news things!</HeadingStyledThree>
-            <div>
-              <a
-                href="https://www.linkedin.com/in/carlos-junio26/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <BsLinkedin />
-              </a>
-              <a
-                href="https://github.com/CarlosX26"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <BsGithub />
-              </a>
-            </div>
-          </div>
+        <HeadingStyledThree>Always news things!</HeadingStyledThree>
+
+        <div>
+          <a
+            href="https://www.linkedin.com/in/carlos-junio26/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsLinkedin />
+          </a>
+          <a
+            href="https://github.com/CarlosX26"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsGithub />
+          </a>
         </div>
       </ContainerStyled>
     </FooterStyled>
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -6,43 +6,16 @@ export const FooterStyled = styled.footer`
   & > div {
     padding: 32px 0;
     display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  .box-info > img {
-    width: 240px;
-    height: 240px;
-    border-radius: 50%;
-    object-fit: cover;
-    object-position: center;
-  }
-  .box-info {
-    display: flex;
-    gap: 16px;
-  }
-  .box-info > div {
-    display: flex;
     flex-direction: column;
     gap: 16px;
   }
-  .box-info > div > div {
+
+  & > div > div {
     display: flex;
     gap: 16px;
   }
-  .box-info > div > div > a {
+  & > div > div > a {
     color: var(--color-gray-1);
     font-size: 32px;
-  }
-
-  @media (max-width: 868px) {
-    & > div {
-      flex-direction: column;
-      align-items: stretch;
-      padding-bottom: 16px;
-    }
-    .box-info > img {
-      display: none;
-    }
   }
 `
