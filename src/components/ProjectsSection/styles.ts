8772cd67f4ca2eb3737a5f43b0667c516acc4e4c
Carlos Junio <cjunior1@live.com>
2023-05-04 11:12:08 -0300

    fix: padding title

--- src/components/ProjectsSection/styles.ts
+++ src/components/ProjectsSection/styles.ts
@@ -4,4 +4,8 @@ export const ProjectsSectionStyled = styled.section`
   min-height: 100vh;
 
   background: linear-gradient(0deg, var(--color-brand-3), var(--color-brand-2));
+
+  & > div > h1 {
+    padding-top: 32px;
+  }
 `
